d548de61ace107d2523e5fe0cdd3c28b6683d7d9
Johan Commelin <johan@commelin.net>
2019-02-12 19:56:00 +0100

    Initial commit

---
+++ src/page01.lean
@@ -0,0 +1,15 @@
+/-                               2019-02-??, Oberhamersbach
+
+Computer-assisted theorem proving — my experience
+
+by Johan Commelin -/
+
+#check "hello, world!"
+
+#check 3
+
+#check ℤ
+
+#check 3 = 4
+
+#check ℤ = "hi"
---
+++ src/page02.lean
@@ -0,0 +1,10 @@
+import tactic.tidy
+import data.complex.exponential
+
+example : 3 = 3 :=
+by simp
+
+open complex
+
+example : exp 0 = 1 :=
+by simp
